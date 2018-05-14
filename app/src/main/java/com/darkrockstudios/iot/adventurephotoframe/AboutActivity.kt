5c2642f8bab20e33feefef1f12e91d695937240e
Wavesonics <adamwbrown@gmail.com>
2018-05-13 20:33:15 -0700

    Added the ability to select different Update channels

--- app/src/main/java/com/darkrockstudios/iot/adventurephotoframe/AboutActivity.kt
+++ app/src/main/java/com/darkrockstudios/iot/adventurephotoframe/AboutActivity.kt
@@ -1,16 +1,34 @@
 package com.darkrockstudios.iot.adventurephotoframe
 
+import android.content.Context
 import android.os.Bundle
 import android.support.annotation.LayoutRes
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.darkrockstudios.iot.adventurephotoframe.application.App
 import com.darkrockstudios.iot.adventurephotoframe.base.BaseActivity
+import com.google.android.things.update.UpdateManager
 import kotlinx.android.synthetic.main.activity_about.*
 import me.eugeniomarletti.extras.SimpleActivityCompanion
+
 
 class AboutActivity : BaseActivity()
 {
 	companion object : SimpleActivityCompanion(AboutActivity::class)
+
+	sealed class UpdateChannels(val channelName: String, channelNameResource: Int, context: Context)
+	{
+		val channelDisplayName = context.getString(channelNameResource)
+
+		override fun toString(): String = channelDisplayName
+
+		class StableChannel(context: Context) : UpdateChannels("stable-channel", R.string.ABOUT_update_channel_stable, context)
+		class BetaChannel(context: Context) : UpdateChannels("beta-channel", R.string.ABOUT_update_channel_beta, context)
+		class DevChannel(context: Context) : UpdateChannels("dev-channel", R.string.ABOUT_update_channel_dev, context)
+		class CanaryChannel(context: Context) : UpdateChannels("canary-channel", R.string.ABOUT_update_channel_canary, context)
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -22,7 +40,55 @@ class AboutActivity : BaseActivity()
 		ABOUT_id.text = getString(R.string.SETTINGS_id, App.inst.photoFrameId)
 
 		ABOUT_welcome_rerun.setOnClickListener(this::onWeclomeClick)
-		ABOUT_reboot.setOnClickListener(this::onRebootClick)
+
+		setupUpdateChannels()
+	}
+
+	private fun setupUpdateChannels()
+	{
+		val updateChannels = if (BuildConfig.DEBUG)
+		{
+			arrayOf(UpdateChannels.StableChannel(this),
+					UpdateChannels.BetaChannel(this),
+					UpdateChannels.DevChannel(this),
+					UpdateChannels.CanaryChannel(this))
+		}
+		else
+		{
+			arrayOf(UpdateChannels.StableChannel(this),
+					UpdateChannels.BetaChannel(this))
+		}
+
+		ABOUT_update_channel_spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, updateChannels)
+
+		var initialIndex = 0
+		// Get the index of the currently selected channel
+		val updateManager = UpdateManager.getInstance()
+		for ((index, channel) in updateChannels.withIndex())
+		{
+			if (channel.channelName == updateManager.channel)
+			{
+				initialIndex = index
+			}
+		}
+
+		Log.d("about", "Current update channel: " + updateManager.channel)
+
+		ABOUT_update_channel_spinner.setSelection(initialIndex, false)
+
+		ABOUT_update_channel_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener
+		{
+			override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long)
+			{
+				val selectedItem = parent.getItemAtPosition(position) as UpdateChannels
+				UpdateManager.getInstance().channel = selectedItem.channelName
+			}
+
+			override fun onNothingSelected(parent: AdapterView<*>)
+			{
+
+			}
+		}
 	}
 
 	@get:LayoutRes
@@ -33,10 +99,5 @@ class AboutActivity : BaseActivity()
 	{
 		WelcomeActivity.start(this)
 		finish()
-	}
-
-	fun onRebootClick(view: View)
-	{
-
 	}
 }
